fee4fc61a918054f5b6b5876591b4ade1a5df142
Rafael Silva <73067778+RafaeI11@users.noreply.github.com>
2021-10-19 20:59:23 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,56 +13,6 @@ from moviepy.editor import ImageClip
 from PIL import Image, ImageTk
 
 
-class Recorder:
-    def __init__(self) -> None:
-        self.img = Image.new("RGB", (80, 80), (0, 0, 0))
-        self.img = numpy.array(self.img)
-        self.frequency = 44100
-        self.seconds = 600
-        self.is_recording = False
-
-    def start_rec(self):
-        self.file_name = (
-            str(datetime.now()).split(".")[0].replace(" ", "_").replace(":", "-")
-        )
-        self.recording = sd.rec(
-            int(self.seconds * self.frequency), samplerate=self.frequency, channels=2
-        )
-        self.start = default_timer()
-        self.is_recording = True
-        print("started recording...")
-
-    def stop_rec(self, progress_bar):
-        print("stopped recording...")
-        sd.stop()
-        self.is_recording = False
-        self.end = default_timer()
-        self.final = int(self.end - self.start) + 1
-        self.audio = AudioClip.AudioArrayClip(self.recording, self.frequency)
-        self.audio.duration = self.final
-        self.clip = ImageClip(self.img, duration=self.final)
-        self.clip.fps = 1
-        self.final_clip = self.clip.set_audio(self.audio)
-        self.final_clip.write_videofile(f"{self.file_name}.mp4")
-        progress_bar.destroy()
-        messagebox.showinfo(
-            "Process Finnished!", f"{self.file_name} created sucefully!"
-        )
-
-    def create_from_audio(self, path, progress_bar):
-        self.audio = AudioFileClip(path)
-        self.clip = ImageClip(self.img, duration=self.audio.duration)
-        self.clip.fps = 1
-        self.final_clip = self.clip.set_audio(self.audio)
-        name, ext = path.split("\\")[-1].split(".")
-        if ext == "mp4":
-            self.final_clip.write_videofile(f"{name} audio.mp4")
-        else:
-            self.final_clip.write_videofile(f"{name}.mp4")
-        progress_bar.destroy()
-        messagebox.showinfo("Process Finnished!", f"{name} created sucefully!")
-
-
 class BarWindow(Toplevel):
     def __init__(self):
         Toplevel.__init__(self)
@@ -81,7 +31,7 @@ class BarWindow(Toplevel):
         self._offsetx = event.x
         self._offsety = event.y
 
-    def pg_bar(self):
+    def pg_bar(self) -> None:
         self.bar = Progressbar(
             self, orient="horizontal", length=200, mode="indeterminate"
         )
@@ -89,10 +39,56 @@ class BarWindow(Toplevel):
         self.bar.start(5)
 
 
-def resource_path(relative_path):
-    """Get absolute path to resource, works for dev and for PyInstaller"""
+class Recorder:
+    def __init__(self):
+        self.img = Image.new("RGB", (80, 80), (0, 0, 0))
+        self.img = numpy.array(self.img)
+        self.frequency = 44100
+        self.seconds = 600
+        self.is_recording = False
+
+    def start_rec(self) -> None:
+        self.file_name = (
+            str(datetime.now()).split(".")[0].replace(" ", "_").replace(":", "-")
+        )
+        self.recording = sd.rec(
+            int(self.seconds * self.frequency), samplerate=self.frequency, channels=2
+        )
+        self.start = default_timer()
+        self.is_recording = True
+
+    def stop_rec(self, progress_bar: BarWindow) -> None:
+        sd.stop()
+        self.is_recording = False
+        self.end = default_timer()
+        self.final = int(self.end - self.start) + 1
+        self.audio = AudioClip.AudioArrayClip(self.recording, self.frequency)
+        self.audio.duration = self.final
+        self.clip = ImageClip(self.img, duration=self.final)
+        self.clip.fps = 1
+        self.final_clip = self.clip.set_audio(self.audio)
+        self.final_clip.write_videofile(f"{self.file_name}.mp4")
+        progress_bar.destroy()
+        messagebox.showinfo(
+            "Process Finnished!", f"{self.file_name} created sucefully!"
+        )
+
+    def create_from_audio(self, path: str, progress_bar: BarWindow) -> None:
+        self.audio = AudioFileClip(path)
+        self.clip = ImageClip(self.img, duration=self.audio.duration)
+        self.clip.fps = 1
+        self.final_clip = self.clip.set_audio(self.audio)
+        name, ext = path.split("\\")[-1].split(".")
+        if ext == "mp4":
+            self.final_clip.write_videofile(f"{name} audio.mp4")
+        else:
+            self.final_clip.write_videofile(f"{name}.mp4")
+        progress_bar.destroy()
+        messagebox.showinfo("Process Finnished!", f"{name} created sucefully!")
+
+
+def resource_path(relative_path: str) -> str:
     try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
@@ -100,11 +96,11 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-def display_img(img, size=(100, 100)):
+def display_img(img: str, size=(100, 100)) -> ImageTk.PhotoImage:
     displayed_img = Image.open(img)
     displayed_img = displayed_img.resize(size, Image.ANTIALIAS)
     displayed_img = ImageTk.PhotoImage(displayed_img)
     return displayed_img
 
 
-ICON = resource_path("images/icon.ico")
+ICON: str = resource_path("images/icon.ico")
